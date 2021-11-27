202c7a481d1a40f1246219aa5bd160a2c2d6f680
Matthew Savin <matt.asavin@gmail.com>
2021-11-27 19:54:08 +0000

    implemented button press logic

--- main.py
+++ main.py
@@ -1,24 +1,36 @@
 import pygame.locals
 from button import *
 
+LOGIN_BUTTON_X = 100
+LOGIN_BUTTON_Y = 300
+EXIT_BUTTON_X = 300
+EXIT_BUTTON_Y = 300
+BUTTON_WIDTH = 150
+BUTTON_HEIGHT = 50
+
 
 def update(dt):
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            if True:
-                print("MOUSEBUTTONDOWN")
+            mouse = pygame.mouse.get_pos()
+            if LOGIN_BUTTON_X <= mouse[0] <= LOGIN_BUTTON_X + BUTTON_WIDTH and \
+                    LOGIN_BUTTON_Y <= mouse[1] <= LOGIN_BUTTON_Y + BUTTON_HEIGHT:
+                print("hit login button")
+            elif EXIT_BUTTON_X <= mouse[0] <= EXIT_BUTTON_X + BUTTON_WIDTH and \
+                    EXIT_BUTTON_Y <= mouse[1] <= EXIT_BUTTON_Y + BUTTON_HEIGHT:
+                pygame.quit()
+                exit()
 
 
 def draw(screen):
     screen.fill((0, 0, 0))
 
     button = Button(screen)
-    button.draw_button(150, 50, 100, 50, "Hello", "Comic Sans MS")
-    button.draw_button(150, 50, 300, 50, "Hello", "Comic Sans MS")
+    button.draw_button(BUTTON_WIDTH, BUTTON_HEIGHT, LOGIN_BUTTON_X, LOGIN_BUTTON_Y, "Login", "Comic Sans MS")
+    button.draw_button(BUTTON_WIDTH, BUTTON_HEIGHT, EXIT_BUTTON_X, EXIT_BUTTON_Y, "Exit", "Comic Sans MS")
 
     pygame.display.flip()
 
@@ -30,8 +42,7 @@ def create_screen():
     dt = 1 / fps
     fps_clock = pygame.time.Clock()
 
-    res = (640, 480)
-    screen = pygame.display.set_mode(res)
+    screen = pygame.display.set_mode((640, 480))
 
     while True:
         update(dt)
