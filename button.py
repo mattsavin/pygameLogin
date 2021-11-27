1ae38f345450d6075fac633694b13af418f2aec3
Matthew Savin <matt.asavin@gmail.com>
2021-11-27 14:12:01 +0000

    add a button (kinda)

---
+++ button.py
@@ -0,0 +1,12 @@
+import pygame
+
+
+class Button:
+    def __init__(self, screen):
+        self.screen = screen
+
+    def draw_button(self,  width, height, pos_x, pos_y, text, font):
+        pygame.draw.rect(self.screen, [40, 50, 70], [pos_x, pos_y, width, height])
+        pygame_font = pygame.font.SysFont(font, 35)
+        pygame_text = pygame_font.render(text, True, (255, 255, 255))
+        self.screen.blit(pygame_text, (pos_x + 5, pos_y + 10))
--- main.py
+++ main.py
@@ -1,35 +1,41 @@
-import pygame
 import pygame.locals
+from button import *
 
 
-def update(dt):
+def update():
     for event in pygame.event.get():
-
-        if event.type == pygame.locals.QUIT:
+        if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            if True:
+                print("MOUSEBUTTONDOWN")
 
 
 def draw(screen):
     screen.fill((0, 0, 0))
 
-    # Do cool stuff
+    button = Button(screen)
+    button.draw_button(50, 50, 140, 40, "Hello", "Comic Sans MS")
 
     pygame.display.flip()
 
 
 def create_screen():
     pygame.init()
+
     fps = 60
     dt = 1 / fps
     fps_clock = pygame.time.Clock()
+    dt = fps_clock.tick()
+
     res = (640, 480)
     screen = pygame.display.set_mode(res)
-    while True:
-        update(dt)
-        draw(screen)
 
-        dt = fps_clock.tick()
+    while True:
+        update()
+        draw(screen)
 
 
 if __name__ == "__main__":
