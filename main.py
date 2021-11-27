900bca02baa38bcc49dc74e5a15ae25b0ca320e9
Matthew Savin <matt.asavin@gmail.com>
2021-11-27 12:54:44 +0000

    Add pygame boilerplate

--- main.py
+++ main.py
@@ -1,2 +1,36 @@
-if __name__ == '__main__':
-    pass
+import pygame
+import pygame.locals
+
+
+def update(dt):
+    for event in pygame.event.get():
+
+        if event.type == pygame.locals.QUIT:
+            pygame.quit()
+            exit()
+
+
+def draw(screen):
+    screen.fill((0, 0, 0))
+
+    # Do cool stuff
+
+    pygame.display.flip()
+
+
+if __name__ == "__main__":
+    pygame.init()
+
+    fps = 60
+    fpsClock = pygame.time.Clock()
+
+    width, height = 640, 480
+    screen = pygame.display.set_mode((width, height))
+
+    dt = 1 / fps
+    while True:
+        update(dt)
+        draw(screen)
+
+        dt = fpsClock.tick()
+
